ca5a89397cf3a431ba79ff361d2fd5c4131aa4b4
rikhallar <forlidzen@gmail.com>
2017-12-24 17:15:43 +0200

    BalancedParentheses problem tests
    Set up and the tests for the balance parentheses string task

---
+++ test_balanced_parentheses.py
@@ -0,0 +1,93 @@
+import balanced_parentheses
+import unittest
+
+
+class TestBalancedParentheses(unittest.TestCase):
+
+    def test_returns_false_when_unbalanced_mixed_group(self):
+        line = "[{)](}"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(False, result)
+
+    def test_returns_true_when_balanced_mixed_group(self):
+        line = "{()}[[{}]]"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_true_on_balance_of_separate_pairs_of_braces(self):
+        line = "{}{}"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_true_on_balance_of_inclusive_braces(self):
+        line = "{{}}"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_true_on_balance_of_two_braces(self):
+        line = "{}"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_true_on_balance_of_separate_pairs_of_brackets(self):
+        line = "[][]"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_true_on_balance_of_inclusive_brackets(self):
+        line = "[[]]"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_true_on_balance_of_two_brackets(self):
+        line = "[]"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_true_on_balance_of_separate_pairs_of_parentheses(self):
+        line = "()()"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_true_on_balance_of_inclusive_parentheses(self):
+        line = "(())"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_false_when_parentheses_not_opened(self):
+        line = ")()"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(False, result)
+
+    def test_returns_false_when_parentheses_not_closed(self):
+        line = "(()"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(False, result)
+
+    def test_returns_true_on_balance_of_two_parentheses(self):
+        line = "()"
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+    def test_returns_true_on_void_string(self):
+        line = ""
+        algorithm = balanced_parentheses.BalancedParentheses()
+        result = algorithm.answer(line)
+        self.assertEqual(True, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
