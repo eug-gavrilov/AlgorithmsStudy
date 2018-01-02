83b7dacadccc21ba8f8de4cbcaf8f5a3d2f648b0
Marlave <forlidzen@gmail.com>
2018-01-02 20:34:48 +0200

    combined_number: 4/6 tests (power of RNG)

--- combined_number.py
+++ combined_number.py
@@ -7,7 +7,19 @@
 # given [5, 50, 56]   it returns "56550"    (56 + 5 + 50)
 # given [420, 42, 423] it returns "42423420" (42 + 423 + 420)
 
-
+from random import random
+def random_key(element):
+    return random()
 class CombinedNumber:
     def answer(self, numbers):
-        return ""
+        a = sorted(numbers, reverse= True)
+        a_1 = sorted(numbers, key=random_key)
+        answer = ""
+        answer_2 = ""
+        for x in a:
+           answer = answer + str(x)
+        for y in a_1:
+            answer_2 = answer_2 +str(y)
+        if answer > answer_2:
+            return answer
+        return answer_2
